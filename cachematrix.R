e221d9f2fe02d13dfad828a53c5cb2aa1192f474
jhconley <jconley7001@gmail.com>
2014-07-23 12:21:03 -0400

    Assignment Completion
    Implementation of makeCacheMatric and cachSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions that support the caching
+## of matrix inversions: one creating an object 
+## to store a matrix and its inversion;
+## the other to compute-and-save or retrieve
+## the inversion using this object.
 
-## Write a short comment describing this function
+## a function that creates a matrix container  
+## that can cache the matrix inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(m = matrix()) {
+  mi <- NULL
+  set <- function(y) {
+    m <<- y
+    mi <<- NULL
+  }
+  get <- function() m
+  setinverse <- function(inverse) mi <<- inverse
+  getinverse <- function() mi
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## a function that returns the inverse of a given matrix.
+## if the matrix container has the inverse, use it; 
+## otherwise, compute the inverse and store the result 
+## in the container before returning it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  mi <- x$getinverse()
+  if(!is.null(mi)) {
+    message("getting cached data")
+    return(mi)
+  }
+  m <- x$get()
+  if (det(m) != 0) {
+    mi <- solve(m)
+    x$setinverse(mi)
+  } else { ## the matrix is singular
+    ## e.g. matrix(1:100, c(10,10)) 
+    ##          message("the matrix is not invertible")
+  }
+  mi
 }
+
